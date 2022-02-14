d32aa35d01d2adc064d94f90e15e830bdf7ea5be
吴博聪 <wubocong@chinatelecom.cn>
2022-02-14 13:49:34 +0800

    fix: 增加跳过按钮，优化代码

--- src/index.js
+++ src/index.js
@@ -2,68 +2,57 @@
 import { isTouchScreen } from './js/isTouchScreen'
 import './css/styles.scss';
 
+// prevent arrow scrolling in firefox
 $(window).on('keydown', function (e) {
   if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
     e.preventDefault();
   }
 });
-
 $(window).on('load', function () {
   $('.page-loader').hide();
   $('.prompt').show();
   $('.starwars').show();
-  // prevent arrow scrolling in firefox
-  
 
   let el = $('.starwars');
   let audio = el.find('audio')[0];
-  let animation = el.find('.animation');
+  let animation = $('#animation-container');
   let cloned = animation.clone(true);
   animation.remove();
   animation = cloned;
 
-  audio.oncanplay = function () {
-    function play() {
-      $('.prompt').hide();
-      $(window).off('click');
-      $(window).off('touchend');
-      $('.page-hide').hide();
-      $('.page-loader').hide(); // grants the loader to hide. Sometimes doesn't hide, maybe due to history navigation in browser.
-      $('body').removeClass('running');
-      $('body').addClass('running');
-      $('body').scrollTop(0);
-      audio.play();
-      el.append(animation);
+  let count = 0;
+  let timer;
+  function skip() {
+    count++;
+    if (count >= 2) $('.skip').addClass('active');
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      count = 0;
+      $('.skip').removeClass('active');
+    }, 3000);
+  }
 
-      // adjust animation speed
-      var titles = $('.titles > div', animation).get(0);
-      if (titles.offsetHeight > 1977) // 1977 is year of the first Star Wars movie.
-      {
-        var exceedSize = titles.offsetHeight - 1977;
-        var animConstant = 0.04041570438799076;
-        var animDist = 20 - exceedSize * animConstant;
-        var cssRule;
-        var ss = document.styleSheets;
-        for (var i = 0; i < ss.length; ++i) {
-          // loop through all the rules!
-          for (var x = 0; x < ss[i].cssRules.length; ++x) {
-            var rule = ss[i].cssRules[x];
-            if (rule.name == "titles" && rule.type == CSSRule.KEYFRAMES_RULE) {
-              cssRule = rule;
-            }
-          }
-        }
-        if (cssRule)
-          cssRule.appendRule("100% { top: " + animDist + "% }");
-      }
-    }
+  function play() {
+    $(window).on('click', skip);
+    $('.prompt').hide();
+    $('.page-hide').hide();
+    $('.page-loader').hide(); // grants the loader to hide. Sometimes doesn't hide, maybe due to history navigation in browser.
+    $('body').removeClass('running');
+    $('body').addClass('running');
+    $('body').scrollTop(0);
+    audio.play();
+    el.append(animation);
+  }
+  audio.oncanplay = function () {
     if (!isTouchScreen()) {
-      $(window).on('click', play);
+      $(window).one('click', play);
     } else {
-      $(window).on('touchend', play);
+      $(window).one('touchend', play);
     }
   };
   audio.onended = function () {
+    $('.skip').removeClass('active');
+    $(window).off('click', skip);
     $('.starwars').hide();
     $('.page-later').show();
   };
